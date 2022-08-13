617f30c8ea082ae1d7b0aa63e5005c396c73e43a
unknown <vitoria1065@outlook.com>
2022-08-13 16:56:44 -0300

    ExerciciosKotlin

---
+++ HelloWord/src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+
+
+import java.util.Scanner
+
+fun main() {
+
+    var excesso1=  0
+    var multa2  = 0
+
+
+
+    println("Digite o peso dos Tomates: ")
+
+
+    excesso1 = - 50
+
+    if (excesso1 > 0) {
+
+        multa2 = 4 * excesso1
+
+        println( o valor da multa é + multa2 + " pois passou " + excesso1 + " quilos De excesso")
+
+    } else {
+
+        multa1 = 0
+         = 0
+
+        println("O valor da multa é: R$" + Multa + " pois teve " + Excesso + " quilos de excesso!!!")
+
+        }
+
+
+
+
+
+
+
+
+}
